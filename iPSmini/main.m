9f56809f4a4ec6f626d2fde2549e6d944a6d8957
Kazumi Harada <kazumi.h.248@gmail.com>
2012-12-05 10:43:53 +0900

    Initial Commit

---
+++ iPSmini/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  iPSmini
+//
+//  Created by 原田　一美 on 2012/12/05.
+//  Copyright (c) 2012年 原田　一美. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
