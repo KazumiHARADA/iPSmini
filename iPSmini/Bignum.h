c05c0735a02f4be1e9098dd1ea139a9134ffe699
Kazumi Harada <kazumi.h.248@gmail.com>
2012-12-11 14:16:28 +0900

    murimuri

---
+++ iPSmini/Bignum.h
@@ -0,0 +1,19 @@
+//
+//  Bignum.h
+//  iPSmini
+//
+//  Created by 原田　一美 on 2012/12/11.
+//  Copyright (c) 2012年 原田　一美. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Bignum : NSObject
+{
+    NSMutableArray *big_num;
+}
+
+
+
+
+@end
---
+++ iPSmini/Bignum.m
@@ -0,0 +1,13 @@
+//
+//  Bignum.m
+//  iPSmini
+//
+//  Created by 原田　一美 on 2012/12/11.
+//  Copyright (c) 2012年 原田　一美. All rights reserved.
+//
+
+#import "Bignum.h"
+
+@implementation Bignum
+
+@end
